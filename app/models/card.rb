674ffd089097a2637b18a85f8459851408214b30
TrypolskyiS <tripolskys@gmail.com>
2017-03-08 20:18:07 +0200

    Added validations and automatical time setting before creation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,22 @@
 class Card < ApplicationRecord
-  validates :original_text,   presence: true
-  validates :translated_text, presence: true
+  validates :original_text,   presence: true, format: { with: /\A[a-zA-Zа-яА-ЯёЁ]+\z/,
+    message: "Only letters can be used" }
+  validates :translated_text, presence: true, format: { with: /\A[a-zA-Zа-яА-ЯёЁ]+\z/,
+    message: "Only letters can be used" }
+  validates :review_date, format: { with: /\A[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])\z/,
+    message: "Incorrect date format" }
+  validate :original_cannot_be_similar_to_translated
+
+  def original_cannot_be_similar_to_translated
+    if self.original_text.downcase == self.translated_text.downcase
+      self.errors[:base] << 'Original text cannot be similar to the translated one.'
+    end
+  end
+
+  before_validation :set_review_date
+
+  protected
+  def set_review_date
+    self.review_date = Time.now + 3.days
+  end
 end
