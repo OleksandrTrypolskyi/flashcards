81b7c911ee0b7bfb5b5ee9bb42d235646e202edf
TrypolskyiS <tripolskys@gmail.com>
2017-03-09 18:19:47 +0200

    Return validation in Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,11 +1,11 @@
 class Card < ApplicationRecord
-#  validates :original_text,   presence: true, format: { with: /\A[a-zA-Zа-яА-ЯёЁ]+\z/,
-#    message: "Only letters can be used" }
-#  validates :translated_text, presence: true, format: { with: /\A[a-zA-Zа-яА-ЯёЁ]+\z/,
-#    message: "Only letters can be used" }
-#  validates :review_date, format: { with: /\A[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])\z/,
-#    message: "Incorrect date format" }
-#  validate :original_cannot_be_similar_to_translated
+  validates :original_text,   presence: true, format: { with: /\A[a-zA-Zа-яА-ЯёЁ]+\z/,
+    message: "Only letters can be used" }
+  validates :translated_text, presence: true, format: { with: /\A[a-zA-Zа-яА-ЯёЁ]+\z/,
+    message: "Only letters can be used" }
+  validates :review_date, format: { with: /\A[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])\z/,
+    message: "Incorrect date format" }
+  validate :original_cannot_be_similar_to_translated
 
   def original_cannot_be_similar_to_translated
     if self.original_text.downcase == self.translated_text.downcase
