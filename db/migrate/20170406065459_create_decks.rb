ebf5e4e7074883e74f7704e007f49b0ed0ae6c7d
TrypolskyiS <tripolskys@gmail.com>
2017-04-10 20:30:41 +0300

    Correct little issue with migratons for coorect Travis work

--- db/migrate/20170406065459_create_decks.rb
+++ db/migrate/20170406065459_create_decks.rb
@@ -8,6 +8,6 @@ class CreateDecks < ActiveRecord::Migration[5.0]
     add_column :cards, :deck_id, :integer
     add_column :users, :current_deck_id, :integer
     add_attachment :cards, :picture
-    add_index :cards, :user_id
+    #add_index :cards, :user_id
   end
 end
