ee33c96696fece7e73aa9dd80d55e4fb75a210ca
TrypolskyiS <tripolskys@gmail.com>
2017-04-23 16:07:43 +0300

    Correct hidden_field.js

--- app/assets/javascripts/hidden_field.js
+++ app/assets/javascripts/hidden_field.js
@@ -1,34 +1,14 @@
-function firstFunction() {
-    $('#time').val(5);
+function firstFunction(score) {
+  $('#time').val(score)
+}
+function secondFunction()  {
+  var time = 1000;
+  var score = 5;
+  for(i = 0; i < 6; i++ ) {
+    setTimeout(firstFunction, time, score)
+    time = time + 20000;
+    score--;
+  }
 }
 
-function secondFunction() {
-    $('#time').val(4);
-}
-
-function thirdFunction() {
-    $('#time').val(3);
-}
-
-function fourthFunction() {
-    $('#time').val(2);
-}
-
-function fifthFunction() {
-    $('#time').val(1);
-}
-
-function sixthFunction() {
-    $('#time').val(0);
-}
-
-function oneFunc() {
-  setTimeout(firstFunction, 1000);
-  setTimeout(secondFunction, 20000);
-  setTimeout(thirdFunction, 60000);
-  setTimeout(fourthFunction, 90000);
-  setTimeout(fifthFunction, 120000);
-  setTimeout(sixthFunction, 180000);
-}
-
-oneFunc();
+secondFunction();
